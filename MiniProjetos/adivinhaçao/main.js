2e636f8c41c0ab04560d196506358602b7beed6b
DANI <romao@alunos.utfpr.edu.br>
2025-09-17 15:46:01 -0300

    Add initial files for guessing game mini project
    Created HTML, CSS, and JS files for a simple guessing game SPA with categories for famous people, verbs, and countries. Includes basic layout, navigation, and initial class structure for game logic.

---
+++ MiniProjetos/adivinhaçao/main.js
@@ -0,0 +1,8 @@
+// - Conceitos: `Math.random`, laços de repetição, `prompt`, `alert`.
+// - Extra: adicionar tentativas limitadas e feedback (maior/menor).
+
+class adivinhacao {
+    constructor(palavra){
+        this.palavra = palavra
+    }
+}
